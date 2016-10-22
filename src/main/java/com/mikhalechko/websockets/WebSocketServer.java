e174cddbd5da837d95cb19c06a5bb376e3208af6
0vector0 <0vector0@gmail.com>
2016-10-22 15:59:14 +0300

    add chat

--- src/main/java/com/mikhalechko/websockets/WebSocketServer.java
+++ src/main/java/com/mikhalechko/websockets/WebSocketServer.java
@@ -1,18 +1,21 @@
 package com.mikhalechko.websockets;
 
 
-import javax.servlet.http.HttpSession;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ServerEndpoint("/app")
 public class WebSocketServer {
 
+    private static Map<String, Session> users = new HashMap<String, Session>();
+
     @OnOpen
     public void onOpen(Session session, EndpointConfig config) throws IOException {
-        session.getBasicRemote().sendText("connected");
-        HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+//        session.getBasicRemote().sendText("connected");
+//        HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
     }
 
     @OnClose
@@ -22,7 +25,25 @@ public class WebSocketServer {
 
     @OnMessage
     public void onMessage(String message, Session session) throws IOException {
-        message += " from server";
-        session.getBasicRemote().sendText(message);
+        String[] messageArrary = message.split(":");
+        if (messageArrary[0].equals("name")) {
+            users.put(messageArrary[1], session);
+        } else if (messageArrary[0].equals("list")) {
+            String output = "list:";
+            for (String name : users.keySet()) {
+                output += name + "\n";
+            }
+            session.getBasicRemote().sendText(output);
+        } else {
+            String nameFrom = "";
+            for (Map.Entry<String, Session> entry : users.entrySet()) {
+                if (entry.getValue() == session) {
+                    nameFrom = entry.getKey();
+                    break;
+                }
+            }
+            String outputMessage = nameFrom + ":" + messageArrary[1];
+            users.get(messageArrary[0]).getBasicRemote().sendText(outputMessage);
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,23 +3,35 @@
 <head>
     <title>Star page</title>
     <script type="text/javascript">
+        var flag = null;
         var socket = new WebSocket("ws://localhost:8080/app");
         socket.onopen = function () {
-            alert("connected");
+//            alert("connected");
+            flag = window.setInterval(sendList, 5000);
         };
         socket.onclose = function (event) {
+            window.clearInterval(flag);
             if (event.wasClean) {
-                alert('close');
+                alert('Closed');
             } else {
-                alert('error');
+                alert('Error');
             }
         };
         socket.onmessage = function (event) {
             var message = event.data;
-            var text = document.getElementById("output").value;
-            text += "\n" + message;
-            document.getElementById("output").value = text;
+            var message_array = message.split(":");
+            if (message_array[0] == "list") {
+                document.getElementById("active").value = message_array[1];
+            } else {
+                var text = document.getElementById("output").value;
+                text += "\n" + message_array[0] + " : " + message_array[1];
+                document.getElementById("output").value = text;
+            }
         };
+
+        function sendList() {
+            socket.send("list:");
+        }
 
         function send() {
             var input = document.getElementById('input').value;
@@ -31,7 +43,8 @@
 <body>
 <h2>WebSocket</h2>
 <form>
-    <textarea id="output"></textarea>
+    <textarea rows="10" cols="45" id="active"></textarea>
+    <textarea rows="10" cols="45" id="output"></textarea>
     <input type="text" id="input"/>
     <input type="button" onclick="send()" name="Send" value="Send"/>
 </form>
